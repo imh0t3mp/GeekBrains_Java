dfc9d5bc2afe345425c902b248070d59cae8d21d
Kurchenkov A.N <a.kurchenkov@dsi.ru>
2019-10-31 17:01:25 +0800

    Вариат решения задачи с использованием коллекций

--- lesson_2/src/main/java/App.java
+++ lesson_2/src/main/java/App.java
@@ -1,3 +1,30 @@
 
 public class App {
+
+    public static void main(String[] args) {
+        TaskCollectionTracker tracker = new TaskCollectionTracker();
+
+        Task t1 = new Task("T1", "Owner1", "Executor1", "Descr1");
+        Task t2 = new Task("T2", "Owner2", "Executor2", "Descr2");
+        Task t3 = new Task("T3", "Owner3", "Executor3", "Descr3");
+        Task t4 = new Task("T4", "Owner4", "Executor4", "Descr4");
+        tracker.addTask(t1);
+        tracker.addTask(t2);
+        tracker.addTask(t3);
+        tracker.addTask(t4);
+        System.out.println("TASK LIST: \n" + tracker.printTaskList());
+        Task t = tracker.getTask(3);
+        System.out.println("FOUND TASK: " + t);
+
+        System.out.println("CHANGE TASK STATUS");
+        t.setStatus(TaskStatus.TODO);
+        System.out.println("TASK LIST: \n" + tracker.printTaskList());
+        System.out.println("DELETE TASK");
+        tracker.deleteTask(t);
+        System.out.println("SEARCH TASK");
+        System.out.println("FOUND TASK: " + tracker.getTask(3));
+        System.out.println("TASK LIST: \n" + tracker.printTaskList());
+        tracker.addTask(new Task("T5", "Owner5", "Executor5", "Descr5"));
+        System.out.println("TASK LIST: \n" + tracker.printTaskList());
+    }
 }
---
+++ lesson_2/src/main/java/TaskCollectionTracker.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskCollectionTracker {
+    private List<Task> taskList;
+    private int capacity = 3;
+
+    /**
+     * Дефолтный конструктор
+     */
+    public TaskCollectionTracker() {
+        taskList = new ArrayList<>();
+    }
+
+    /**
+     * ДОбавить задачу в список
+     *
+     * @param task
+     */
+    public void addTask(Task task) {
+        if (taskList.size() == capacity) {
+            System.err.println("ERROR: Список задач заполнен");
+        } else if (taskList.contains(task)) {
+            System.err.printf("ERROR: Задача " + task.getShortDescription() + " уже есть в списке");
+        } else {
+            taskList.add(task);
+        }
+    }
+
+    /**
+     * Удалить задачу из списка
+     *
+     * @param task - Задача для удаления
+     */
+    public void deleteTask(Task task) {
+        taskList.remove(task);
+    }
+
+    /**
+     * Проверить, есть ли в списке задача с таким ID
+     *
+     * @param id - ID задачи
+     * @return
+     */
+    public boolean hasTask(int id) {
+        for (Task task : taskList) {
+            if (task.getId() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Проверить, есть ли в списке задача с таким ID
+     *
+     * @param taskName - имя задачи
+     * @return
+     */
+    public boolean hasTask(String taskName) {
+        for (Task task : taskList) {
+            if (task.getName().equals(taskName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Получить задачу по ID
+     *
+     * @param id - ID Задачи
+     * @return
+     */
+    public Task getTask(int id) {
+        for (Task task : taskList) {
+            if (task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Получить задачу по её имени
+     * Если в списке есть более одной задачи с таким именем, тогда вернёт первую в списке
+     *
+     * @param taskName - имя задачи
+     * @return
+     */
+    public Task getTask(String taskName) {
+        for (Task task : taskList) {
+            if (task.getName().equals(taskName)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Вывести список задач в строку
+     *
+     * @return - строка со списком задач
+     */
+    public String printTaskList() {
+        StringBuilder sb = new StringBuilder();
+        for (Task task : taskList) {
+            sb.append(task).append("\n");
+        }
+        return sb.toString();
+
+    }
+}
