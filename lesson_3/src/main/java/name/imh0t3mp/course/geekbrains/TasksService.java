1dfb81f08ba012b085067c479580335e03fce9e7
Kurchenkov A.N <a.kurchenkov@dsi.ru>
2019-11-04 11:54:56 +0800

    Добавил универсальный интерфейс для последующей реализации в репозиториях реализующих разные варианты хранения записей: в массиве, в списке, в БД... Добавил имплементацию интерфейса на классе TaskArrayRepositoryImpl - реализующий хранение списка задач в массиве. Работа с репозиторием задач реализована в классе TaskService который реализует набор действий для работы с задачами в репозитории: добавление, удаление, получение задач и вывод списка задач. TODO: Очень хочется добавить механизм работы с исключениями взамен к обработке null-ов и вывода в StdErr текста. Если таковое нужно, вывод текста сообщения об ошибке и обработка ошибок верхнего уровня: нет записи в реестре, переполнение реестра и т.п., нужно выносить на уровень TaskService

---
+++ lesson_3/src/main/java/name/imh0t3mp/course/geekbrains/TasksService.java
@@ -0,0 +1,57 @@
+package name.imh0t3mp.course.geekbrains;
+
+import name.imh0t3mp.course.geekbrains.repository.TaskRepository;
+import name.imh0t3mp.course.geekbrains.repository.impl.TaskArrayRepositoryImpl;
+
+/**
+ * Унифицированный класс для работы с разными типами репозиториев
+ * <p>
+ * TODO: Здеь напрашивается FrontController
+ */
+public class TasksService {
+
+    private TaskRepository repository;
+
+    /**
+     * Дефолтный конструктор
+     */
+    TasksService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * По умолчанию будем считать, что данные храним в репозитории массива
+     */
+    TasksService() {
+        this.repository = new TaskArrayRepositoryImpl();
+    }
+
+
+    public void addTask(Task task) {
+        this.repository.addTask(task);
+    }
+
+    public Task getTask(int id) {
+        return this.repository.getTask(id);
+    }
+
+    public Task getTask(String taskName) {
+        return this.repository.getTask(taskName);
+    }
+
+    public void deleteTask(Task task) {
+        this.repository.deleteTask(task);
+    }
+
+    public void deleteTask(int id) {
+        this.repository.deleteTask(id);
+    }
+
+    public void deleteTask(String taskName) {
+        this.repository.deleteTask(taskName);
+    }
+
+    public String toString() {
+        return this.repository.toString();
+    }
+}
