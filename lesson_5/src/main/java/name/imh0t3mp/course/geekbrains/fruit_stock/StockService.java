6ac19bb3cc98303d901d7bf5985b52680ef416f6
Kurchenkov A.N <a.kurchenkov@dsi.ru>
2019-11-07 22:15:44 +0800

    Ммм, надеюсь работает

--- lesson_5/src/main/java/name/imh0t3mp/course/geekbrains/fruit_stock/StockService.java
+++ lesson_5/src/main/java/name/imh0t3mp/course/geekbrains/fruit_stock/StockService.java
@@ -3,8 +3,6 @@ package name.imh0t3mp.course.geekbrains.fruit_stock;
 import name.imh0t3mp.course.geekbrains.fruit_stock.errors.*;
 import name.imh0t3mp.course.geekbrains.fruit_stock.impl.ArrayStockImpl;
 
-import java.util.Collection;
-
 /**
  * Наверно, здесь нужна фабрика
  */
@@ -35,6 +33,8 @@ public class StockService {
         }
     }
 
+
+
     /**
      * Пересыпать фрукты из коробки b1 в коробку b2
      * Если коробка b2 имеет ёмкосмть меньше чем b1, будет выброшено исключение
@@ -49,23 +49,21 @@ public class StockService {
      * @throws NoFreeSpaceInBox      - если в коробке есть фрукты и места не достаточно
      */
     public void shiftBoxes(Box b1, Box b2)
-            throws WrongTypeOfFruitInBox, WrongBoxCapacity, NoFreeSpaceInBox, BoxIsEmpty {
+            throws WrongTypeOfFruitInBox, WrongBoxCapacity, NoFreeSpaceInBox, BoxIsEmpty { System.out.println(b2.getClass());
         if (b1.getItemsCount() > b2.getCapacity())
             throw new WrongBoxCapacity("В коробке #" + b2.getBoxID() + " не достаточно места для " +
                     "размещения " + b1.getItemsCount() + " элементов из ящика #" + b1.getBoxID());
         if (0 != b2.getItemsCount() && b1.getItemsCount() > (b2.getCapacity() - b2.getItemsCount()))
             throw new NoFreeSpaceInBox("В ящике #" + b2.getBoxID() + " не хватает места для " +
                     "добавления " + b1.getItemsCount() + " элементов ящика #" + b1.getBoxID());
-        System.out.println(b1);
-        System.out.println(b2);
-//        try {
-//            b2.addAllItems((Collection) b1);
-//        } catch (ClassCastException err) {
-//            throw new WrongTypeOfFruitInBox("Ящики содержат разные типы фруктов.\n" +
+        try {
+            b2.shiftItems(b1);
+        } catch (ClassCastException err) {
+            throw new WrongTypeOfFruitInBox("Ящики содержат разные типы фруктов.");//\n" +
 //                    "Ящик 1 содержит " + b1.getOneItem().getCommonType() + "\n" +
 //                    "Ящик 2 содержит " + b2.getOneItem().getCommonType() + "\n"
 //            );
-//        }
+        }
     }
 
     /**
@@ -90,7 +88,7 @@ public class StockService {
      *
      * @param box - ящик для добавления
      */
-    public void addBoxToStock(Box box) {
+    public void addBoxToStock(Box<?> box) {
         try {
             stock.addBox(box);
         } catch (StockIsFull | BoxInStock err) {
@@ -114,7 +112,7 @@ public class StockService {
      * @param box - искомый ящик
      * @return - TRUE|FALSE
      */
-    public boolean hasBoxAtStock(Box box) {
+    public boolean hasBoxAtStock(Box<?> box) {
         return stock.hasBox(box);
     }
 
@@ -125,11 +123,11 @@ public class StockService {
      * @return - вес НЕТТО ящика
      * @throws BoxNotFound - ящик не найден на складе
      */
-    public float getNetto(Box box) throws BoxNotFound {
+    public float getWeight(Box<?> box) throws BoxNotFound {
         if (!stock.hasBox(box))
             throw new BoxNotFound("На складе не найден ящик #" + box.getBoxID() + "\n Возможно на " +
                     "складе ошибка сортамента");
-        return box.getNetto();
+        return 0 == box.getItemsCount() ? 0.0f : box.getWeight();
     }
 
     /**
@@ -139,9 +137,9 @@ public class StockService {
      * @return - вес ящика
      * @throws BoxNotFound - если ящик не найден на складе
      */
-    public float getNetto(String boxId) throws BoxNotFound {
+    public float getWeight(String boxId) throws BoxNotFound {
         if (!stock.hasBox(boxId))
             throw new BoxNotFound("На складе не найден ящик #" + boxId);
-        return stock.getBox(boxId).getNetto();
+        return 0 == stock.getBox(boxId).getItemsCount() ? 0.0f : stock.getBox(boxId).getWeight();
     }
 }
