3a6ba515380d4fc423d2e3e737001f9d692e05e9
Kurchenkov A.N <a.kurchenkov@dsi.ru>
2019-11-22 15:16:27 +0800

    Добавил возможность работы с H2 ImMemory DB

--- task_manager/src/main/java/name/imh0t3mp/course/geekbrains/App.java
+++ task_manager/src/main/java/name/imh0t3mp/course/geekbrains/App.java
@@ -22,9 +22,15 @@ public class App {
         log.debug("Трекер задач на в БД с Hibername");
         Session session = null;
         try (SessionFactory factory = new Configuration()
-                .configure("config/hibernate.cfg.xml")
+                .configure("config/hibernate_h2db.cfg.xml")
                 .buildSessionFactory()) {
             TasksService service = new TasksService(new TaskHibernateRepoImpl(factory));
+            System.out.println("TASK LIST:");
+            System.out.println(service.getAllTasks());
+            service.addTask(new Task("T1","TASK1","OWNER1","EXECUTOR1"));
+            service.addTask(new Task("T2","TASK2","OWNER2","EXECUTOR2"));
+            service.addTask(new Task("T3","TASK3","OWNER3","EXECUTOR3"));
+            service.addTask(new Task("T4","TASK4","OWNER4","EXECUTOR4"));
             System.out.println("TASK LIST:");
             System.out.println(service.getAllTasks());
             System.out.println("GET TASK 'T1'");
