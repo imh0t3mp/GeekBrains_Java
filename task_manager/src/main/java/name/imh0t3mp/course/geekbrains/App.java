ed9a9bf613309c154b0d54e365283b64b2a00225
Kurchenkov A.N <a.kurchenkov@dsi.ru>
2019-11-25 10:36:40 +0800

    Класс, добавил репозиторий для БД

--- task_manager/src/main/java/name/imh0t3mp/course/geekbrains/App.java
+++ task_manager/src/main/java/name/imh0t3mp/course/geekbrains/App.java
@@ -1,11 +1,12 @@
 package name.imh0t3mp.course.geekbrains;
 
 import name.imh0t3mp.course.geekbrains.config.TaskTrackerConfig;
-import name.imh0t3mp.course.geekbrains.task_tracker.Task;
-import name.imh0t3mp.course.geekbrains.task_tracker.TaskStatus;
 import name.imh0t3mp.course.geekbrains.task_tracker.TasksService;
+import name.imh0t3mp.course.geekbrains.task_tracker.repository.impl.TaskDatabaseRepoImpl;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.Connection;
 
 public class App {
     private static Logger log = Logger.getLogger(App.class);
@@ -19,33 +20,35 @@ public class App {
     private static void testSpringDatabase() {
         try (AnnotationConfigApplicationContext context =
                      new AnnotationConfigApplicationContext(TaskTrackerConfig.class)) {
+            Connection con = context.getBean("jdbcConnection", Connection.class);
             log.info("CONTEXT:" + context);
 
             TasksService service = context.getBean("tasksService", TasksService.class);
             log.info("SERVICE:" + service);
-
-            service.addTask(new Task("T1", "Task1", "Owner1", "Executor1"));
-            service.addTask(new Task("T2", "Task2", "Owner1", "Executor2"));
-            service.addTask(new Task("T3", "Task3", "Owner2", "Executor1"));
-            service.addTask(new Task("T4", "Task4", "Owner2", "Executor2"));
-
+            service.setRepository(new TaskDatabaseRepoImpl(con));
+//
+//            service.addTask(new Task("T1", "Task1", "Owner1", "Executor1"));
+//            service.addTask(new Task("T2", "Task2", "Owner1", "Executor2"));
+//            service.addTask(new Task("T3", "Task3", "Owner2", "Executor1"));
+//            service.addTask(new Task("T4", "Task4", "Owner2", "Executor2"));
+//
             log.info("ALL TASKS:");
             log.info(service.getAllTasks());
-
-            log.info("CHANGE TASK STATUS");
-            service.changeTaskStatus("T1", TaskStatus.DECLINED);
-
-            log.info("ALL TASKS:");
-            log.info(service.getAllTasks());
-
-            log.info("ALL TASKS WITH STATUS:" + TaskStatus.DECLINED);
-            log.info(service.searchByStatus(TaskStatus.DECLINED));
-
-            log.info("DELETE TASK:" + service.getTask("T2"));
-            service.deleteTask("T2");
-
-            log.info("ALL TASKS:");
-            log.info(service.getAllTasks());
+//
+//            log.info("CHANGE TASK STATUS");
+//            service.changeTaskStatus("T1", TaskStatus.DECLINED);
+//
+//            log.info("ALL TASKS:");
+//            log.info(service.getAllTasks());
+//
+//            log.info("ALL TASKS WITH STATUS:" + TaskStatus.DECLINED);
+//            log.info(service.searchByStatus(TaskStatus.DECLINED));
+//
+//            log.info("DELETE TASK:" + service.getTask("T2"));
+//            service.deleteTask("T2");
+//
+//            log.info("ALL TASKS:");
+//            log.info(service.getAllTasks());
         }
     }
 }
--- task_manager/src/main/java/name/imh0t3mp/course/geekbrains/config/TaskTrackerConfig.java
+++ task_manager/src/main/java/name/imh0t3mp/course/geekbrains/config/TaskTrackerConfig.java
@@ -1,9 +1,26 @@
 package name.imh0t3mp.course.geekbrains.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 @ComponentScan(basePackages = {"name.imh0t3mp.course.geekbrains.task_tracker"})
 public class TaskTrackerConfig {
+    //    @Bean
+//    public EntityManagerFactory entityManagerFactory(){
+//        return new org.hibernate.cfg.Configuration()
+//                .configure("hibernate.cfg.xml")
+//                .buildSessionFactory();
+//    }
+    @Bean(name = "jdbcConnection")
+    public Connection jdbcConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection con = DriverManager.getConnection("jdbc:sqlite:main.db");
+        return con;
+    }
 }
