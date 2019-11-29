ec5170e58d35ee583eb76148edd2164bb07017e9
Kurchenkov A.N <a.kurchenkov@dsi.ru>
2019-11-29 16:53:51 +0800

    Нус, подготовил всё к выполнению нового Д.З.

---
+++ lesson_14/src/main/java/name/imh0t3mp/geekbrains/course/WebApplication.java
@@ -0,0 +1,17 @@
+package name.imh0t3mp.geekbrains.course;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    private static Logger log = LoggerFactory.getLogger(WebApplication.class);
+
+    public static void main(String[] args) {
+        log.debug(">>>>> APP STARTED");
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
---
+++ lesson_14/src/test/java/name/imh0t3mp/geekbrains/course/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package name.imh0t3mp.geekbrains.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
